e9cd613e06c2256ec43f485a2c7b2d289b9bd799
overlordx88 <47886384+overlordx88@users.noreply.github.com>
2019-08-08 04:07:49 +0430

    Add files via upload

--- 002.js
+++ 002.js
@@ -1,6 +1,11 @@
 var list = ['Zebra', 'Tiger', 'Lion', 'Bird', 'Cat', 'Dog'];
-var consumer = Number(propmt (" What product do you need? : Zebra ,  Tiger ,  Lion ,  Bird ,  Cat ,  Dog "));
-var stack = list.indexOf(consumer);
-var result = (stack == -1) ? alert(" product not available " + consumer) : alert(  "there is in the stack go to buy " + consumer);
-console.log(list.indexOf(consumer));
+console.log(list);
+
+var consumer = Number(propmt(" What product number do you choose 0-6?"));
+// var stack = list.indexOf(consumer);
+console.log("index is: " + consumer);
+console.log("content is: " + list[consumer]);
+alert(" product not available : ( "+ list[consumer]+" )    the item will remove from list! ");
+list.splice(consumer, 1);
+console.log(list);
 
